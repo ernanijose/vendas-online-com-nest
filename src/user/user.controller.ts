bca60f0faa0340ba32c0149198a27a42264009dc
ernanijose <ernanijose@gmail.com>
2024-07-20 19:55:17 -0300

    Criando o service com rotas GET e Post

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,24 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService){
+
+    }
+
     @Get()
-    async getAllUsers(){
-        return JSON.stringify({ teste: 'teste' });
+    async getAllUsers(): Promise<User[]>{
+        return this.userService.getAllUsers();
+    }
+
+    @Post()    
+    async createUser(
+        @Body() createUserDto: CreateUserDto
+    ): Promise<User>{
+        return this.userService.createUser(createUserDto);
     }
 }
